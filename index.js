7917e601893864edcf8707802cdb4ea772636cb4
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-12-11 22:51:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+let random = Math.floor(Math.random()*10) + 1;
+let mod = 10 % 6;
+let max = Math.max(1,20,14); 
